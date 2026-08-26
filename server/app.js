19fe8a39a8828f0990135db83974628774ce0cb8
Jae-Keanu Phinisee <phiniseejae@gmail.com>
2026-08-25 21:51:33 -0500

    feat: add health check endpoint to app.js

--- server/app.js
+++ server/app.js
@@ -10,3 +10,14 @@ const port = process.env.PORT || 4000;
 //Start and allow cross origin from the env file on my ip address 
 app.use(cors({ origin: process.env.CLIENT_ORIGIN || 'http://127.0.0.1:5173' }));
 app.use(express.json());
+
+app.get("/health" , (req, res) => { 
+    res.status(200).json({
+    ok: true,
+    service: "nextrole-api",
+  });
+});
+
+
+
+export default app; 
