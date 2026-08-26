478de37068abaa3fed35658f5e6536f9740d84cf
Jae-Keanu Phinisee <phiniseejae@gmail.com>
2026-08-25 21:50:06 -0500

    Added in server start logic separting index.js and app.js's logic so that the app handles express server starting and api use

---
+++ server/app.js
@@ -0,0 +1,12 @@
+import cors from 'cors'; //Cross origin ?? 
+import dotenv from 'dotenv';
+import express from 'express';
+
+//Start Server 
+const app = express() 
+//Initialize port 
+const port = process.env.PORT || 4000;
+
+//Start and allow cross origin from the env file on my ip address 
+app.use(cors({ origin: process.env.CLIENT_ORIGIN || 'http://127.0.0.1:5173' }));
+app.use(express.json());
