4555c35110ada19fc451c2e9d5e88fe754c4e3ca
Jae-Keanu Phinisee <phiniseejae@gmail.com>
2026-08-27 21:04:31 -0500

    feat: migrate from MongoDB to PostgreSQL; implement database connection and query handling

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 
 import 'dotenv/config';
 import  app  from "./app.js";
+import connectDB from "./config/db.js";
+import { ensureApplicationsTable } from "./models/applicationsSchema.js";
+import { ensureUsersTable } from "./models/userSchema.js";
 
 const PORT = process.env.PORT || 4000;
 
+
 try {
+  const connected = await connectDB();
+
+  if (connected) {
+    await ensureUsersTable();
+    await ensureApplicationsTable();
+  }
+
   app.listen(PORT, () => {
     console.log(`NextRole API running on http://localhost:${PORT}`);
   });
