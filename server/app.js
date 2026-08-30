357aa77b572e1614f8eb88f9bd7bed6290a44a40
Jae-Keanu Phinisee <160191731+jaeke131@users.noreply.github.com>
2026-08-29 23:00:22 -0500

    fix: resolve PostgreSQL branch integration issues

--- server/app.js
+++ server/app.js
@@ -1,27 +1,25 @@
-import cors from 'cors'; //Cross origin ?? 
-import dotenv from 'dotenv';
-import express from 'express';
+import cors from "cors";
+import express from "express";
 import applicationRoutes from "./routes/applicationRoute.js";
-import userRoutes from "./routes/userRoute.js"
-//Start Server 
-const app = express() 
-//Initialize port 
-const port = process.env.PORT || 4000;
+import userRoutes from "./routes/userRoute.js";
 
-//Start and allow cross origin from the env file on my ip address 
-app.use(cors({ origin: process.env.CLIENT_ORIGIN || 'http://127.0.0.1:5173' }));
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_ORIGIN || "http://127.0.0.1:5173",
+  })
+);
 app.use(express.json());
 
-app.use("/api/applications", applicationRoutes); 
+app.use("/api/applications", applicationRoutes);
 app.use("/api/users", userRoutes);
 
-app.get("/health" , (req, res) => { 
-    res.status(200).json({
+app.get("/health", (req, res) => {
+  res.status(200).json({
     ok: true,
     service: "nextrole-api",
   });
 });
 
-
-
-export default app; 
+export default app;
